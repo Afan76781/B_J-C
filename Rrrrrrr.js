d3a10772f12f6ccf08baac8a9621bb6a6f15730a
Afan76781 <94213349+Afan76781@users.noreply.github.com>
2023-12-01 15:34:18 +0500

    Create Rrrrrrr.js

---
+++ Rrrrrrr.js
@@ -0,0 +1,199 @@
+var _0x24e5 = ["onload", "lalla store", "website", "assets/img_lalla.jpg", "SEO", "This personal project started as a concept for a small business online fashion store with the name Lalla. The brand\'s identity design was first created and then the website was designed directly with code. This was a project to help me practice, and because I love the challenge of designing things directly in the browser.", "https://www.youtube.com/embed/fgOBjSFCDj8?rel=0&autoplay=1", "landing page", "assets/img_landingpage.jpg", "This personal landing page that I\'ve made to improve my skills. It has a simple and clean design but with a couple of animations and interactions that help the user to have a better and more interesting experience.", "#", "daily ui challenge", "front-end development", "assets/img_daily.jpg", "Daily UI is a series of daily design challenges and I decided to do this on my spare time, to improve my skills as a designer, and on top of that, as a coder. I usually start designing on Figma, and then I develop everything by myself. I think it\'s a good exercise, to know what I can improve in my design process, that will make my coding process easier.", "https://tachibao1210.web.app/dailyui/", "\x0D\x0A                <div class=\"works__item\">\x0D\x0A                    <h4 class=\"works__title\">", "title", " <span>", "</span></h4>\x0D\x0A                    <h5 class=\"works__subtitle\">", "subtitle", "</h5>\x0D\x0A                    <small class=\"works__label\">", "label", "</small>\x0D\x0A                    <figure class=\"works__img\"><img src=\"", "img", "\" alt=\"\"></figure>\x0D\x0A                    <div class=\"works__info\">\x0D\x0A                        <div class=\"works__detail\">\x0D\x0A                            <h6 class=\"works__subtitle2\">tools and languages</h6>\x0D\x0A                            <div class=\"works__tools\"></div>\x0D\x0A                        </div>\x0D\x0A                        <div class=\"works__detail\">\x0D\x0A                            <h6 class=\"works__subtitle2\">about project</h6>\x0D\x0A                            <div class=\"works__description\">", "description", "</div>\x0D\x0A                        </div>\x0D\x0A                        <div class=\"works__link\">\x0D\x0A                            <a href=\"", "url", "\" target=\"_blank\" rel=\"noopenner\">view work</a>\x0D\x0A                        </div>\x0D\x0A                    </div>\x0D\x0A                </div>", "beforeend", "insertAdjacentHTML", ".works__list", "querySelector", "<span>", "</span>", "id", ".works__tools", "querySelectorAll", "forEach", "tools", ".works__item", "click", "addEventListener", "body", ".works__info", "currentTarget", "openWork", "remove", "classList", "height", "style", "0", "scrollHeight", "px", "add", "closest", "target", "pageYOffset", "transform", ".mainHeading__text", "translateX(-", "px)", "intersectionRatio", "y", "intersectionRect", "translateX(", "observe", ".mainTitle.right span", ".mainTitle.left span", ".mainTitle.right .line", ".mainTitle.left .line", "translateY(-", "translateY(", ".about__img", ".about__textContent", "anime", "section", ".mainTitle", ".about__img img", "onscroll"];
+window[_0x24e5[0]] = function (_0x1c54x1) {
+    function _0x1c54x2() {
+        const _0x1c54x3 = [{
+            id: 0,
+            title: _0x24e5[1],
+            subtitle: _0x24e5[2],
+            label: _0x24e5[3],
+            img: _0x24e5[4],
+            alt: _0x24e5[5],
+            tools: [_0x24e5[6], _0x24e5[7], _0x24e5[8], _0x24e5[9], _0x24e5[10], _0x24e5[11]],
+            description: _0x24e5[12],
+            url: _0x24e5[13]
+        }, {
+            id: 1,
+            title: _0x24e5[14],
+            subtitle: _0x24e5[2],
+            label: _0x24e5[15],
+            img: _0x24e5[16],
+            alt: _0x24e5[5],
+            tools: [_0x24e5[6], _0x24e5[7], _0x24e5[8], _0x24e5[9], _0x24e5[10], _0x24e5[11], _0x24e5[17]],
+            description: _0x24e5[18],
+            url: _0x24e5[19]
+        }, {
+            id: 2,
+            title: _0x24e5[20],
+            subtitle: _0x24e5[2],
+            label: _0x24e5[3],
+            img: _0x24e5[21],
+            alt: _0x24e5[5],
+            tools: [_0x24e5[6], _0x24e5[7], _0x24e5[8], _0x24e5[9], _0x24e5[10], _0x24e5[11], _0x24e5[17]],
+            description: _0x24e5[22],
+            url: _0x24e5[23]
+        }, {
+            id: 3,
+            title: _0x24e5[24],
+            subtitle: _0x24e5[25],
+            label: _0x24e5[3],
+            img: _0x24e5[26],
+            alt: _0x24e5[5],
+            tools: [_0x24e5[6], _0x24e5[7], _0x24e5[8], _0x24e5[9], _0x24e5[10], _0x24e5[11]],
+            description: _0x24e5[27],
+            url: _0x24e5[28]
+        }];
+        _0x1c54x3[_0x24e5[52]](function (_0x1c54x4) {
+            let _0x1c54x5 = `${_0x24e5[29]}${_0x1c54x4[_0x24e5[30]]}${_0x24e5[31]}${_0x1c54x4[_0x24e5[30]]}${_0x24e5[32]}${_0x1c54x4[_0x24e5[33]]}${_0x24e5[34]}${_0x1c54x4[_0x24e5[35]]}${_0x24e5[36]}${_0x1c54x4[_0x24e5[37]]}${_0x24e5[38]}${_0x1c54x4[_0x24e5[39]]}${_0x24e5[40]}${_0x1c54x4[_0x24e5[41]]}${_0x24e5[42]}`;
+            document[_0x24e5[46]](_0x24e5[45])[_0x24e5[44]](_0x24e5[43], _0x1c54x5);
+            _0x1c54x4[_0x24e5[53]][_0x24e5[52]](function (_0x1c54x6) {
+                let _0x1c54x7 = `${_0x24e5[47]}${_0x1c54x6}${_0x24e5[48]}`;
+                document[_0x24e5[51]](_0x24e5[50])[_0x1c54x4[_0x24e5[49]]][_0x24e5[44]](_0x24e5[43], _0x1c54x7)
+            })
+        })
+    }
+
+    function _0x1c54x8() {
+        const _0x1c54x9 = document[_0x24e5[51]](_0x24e5[54]);
+        document[_0x24e5[46]](_0x24e5[57])[_0x24e5[56]](_0x24e5[55], _0x1c54xc);
+        _0x1c54x9[_0x24e5[52]](function (_0x1c54x4) {
+            _0x1c54x4[_0x24e5[56]](_0x24e5[55], _0x1c54xa)
+        });
+
+        function _0x1c54xa(_0x1c54x1) {
+            const _0x1c54xb = _0x1c54x1[_0x24e5[59]][_0x24e5[46]](_0x24e5[58]);
+            _0x1c54x9[_0x24e5[52]](function (_0x1c54x4) {
+                _0x1c54x4[_0x24e5[62]][_0x24e5[61]](_0x24e5[60]);
+                _0x1c54x4[_0x24e5[46]](_0x24e5[58])[_0x24e5[64]][_0x24e5[63]] = _0x24e5[65]
+            });
+            _0x1c54xb[_0x24e5[64]][_0x24e5[63]] = _0x1c54xb[_0x24e5[66]] + _0x24e5[67];
+            _0x1c54x1[_0x24e5[59]][_0x24e5[62]][_0x24e5[68]](_0x24e5[60])
+        }
+
+        function _0x1c54xc(_0x1c54x1) {
+            if (_0x1c54x1[_0x24e5[70]][_0x24e5[69]](_0x24e5[54])) {
+                return
+            };
+            _0x1c54x9[_0x24e5[52]](function (_0x1c54x4) {
+                _0x1c54x4[_0x24e5[62]][_0x24e5[61]](_0x24e5[60]);
+                _0x1c54x4[_0x24e5[46]](_0x24e5[58])[_0x24e5[64]][_0x24e5[63]] = _0x24e5[65]
+            })
+        }
+    }
+
+    function _0x1c54xd() {
+        let _0x1c54xe = window[_0x24e5[71]],
+            _0x1c54xf = _0x1c54xe * 0.15,
+            _0x1c54x10 = _0x1c54xe * 0.2;
+        document[_0x24e5[46]](_0x24e5[73])[_0x24e5[64]][_0x24e5[72]] = _0x24e5[74] + _0x1c54xf + _0x24e5[75]
+    }
+
+    function _0x1c54x11() {
+        observer = new IntersectionObserver((_0x1c54x12) => {
+            _0x1c54x12[_0x24e5[52]]((_0x1c54x13) => {
+                if (_0x1c54x13[_0x24e5[76]] > 0) {
+                    let _0x1c54xe = _0x1c54x13[_0x24e5[78]][_0x24e5[77]],
+                        _0x1c54xf = _0x1c54xe * 0.05;
+                    _0x1c54x13[_0x24e5[70]][_0x24e5[64]][_0x24e5[72]] = _0x24e5[79] + _0x1c54xf + _0x24e5[75]
+                }
+            })
+        });
+        observer2 = new IntersectionObserver((_0x1c54x12) => {
+            _0x1c54x12[_0x24e5[52]]((_0x1c54x13) => {
+                if (_0x1c54x13[_0x24e5[76]] > 0) {
+                    let _0x1c54xe = _0x1c54x13[_0x24e5[78]][_0x24e5[77]],
+                        _0x1c54xf = _0x1c54xe * 0.05;
+                    _0x1c54x13[_0x24e5[70]][_0x24e5[64]][_0x24e5[72]] = _0x24e5[74] + _0x1c54xf + _0x24e5[75]
+                }
+            })
+        });
+        observer3 = new IntersectionObserver((_0x1c54x12) => {
+            _0x1c54x12[_0x24e5[52]]((_0x1c54x13) => {
+                if (_0x1c54x13[_0x24e5[76]] > 0) {
+                    let _0x1c54xe = _0x1c54x13[_0x24e5[78]][_0x24e5[77]],
+                        _0x1c54xf = _0x1c54xe * 0.2;
+                    _0x1c54x13[_0x24e5[70]][_0x24e5[64]][_0x24e5[72]] = _0x24e5[74] + _0x1c54xf + _0x24e5[75]
+                }
+            })
+        });
+        observer4 = new IntersectionObserver((_0x1c54x12) => {
+            _0x1c54x12[_0x24e5[52]]((_0x1c54x13) => {
+                if (_0x1c54x13[_0x24e5[76]] > 0) {
+                    let _0x1c54xe = _0x1c54x13[_0x24e5[78]][_0x24e5[77]],
+                        _0x1c54xf = _0x1c54xe * 0.2;
+                    _0x1c54x13[_0x24e5[70]][_0x24e5[64]][_0x24e5[72]] = _0x24e5[79] + _0x1c54xf + _0x24e5[75]
+                }
+            })
+        });
+        document[_0x24e5[51]](_0x24e5[81])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer[_0x24e5[80]](_0x1c54x4)
+        });
+        document[_0x24e5[51]](_0x24e5[82])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer2[_0x24e5[80]](_0x1c54x4)
+        });
+        document[_0x24e5[51]](_0x24e5[83])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer3[_0x24e5[80]](_0x1c54x4)
+        });
+        document[_0x24e5[51]](_0x24e5[84])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer4[_0x24e5[80]](_0x1c54x4)
+        })
+    }
+
+    function _0x1c54x14() {
+        observer = new IntersectionObserver((_0x1c54x12) => {
+            _0x1c54x12[_0x24e5[52]]((_0x1c54x13) => {
+                if (_0x1c54x13[_0x24e5[76]] > 0) {
+                    let _0x1c54xe = _0x1c54x13[_0x24e5[78]][_0x24e5[77]],
+                        _0x1c54xf = _0x1c54xe * 0.05;
+                    _0x1c54x13[_0x24e5[70]][_0x24e5[64]][_0x24e5[72]] = _0x24e5[85] + _0x1c54xf + _0x24e5[75]
+                }
+            })
+        });
+        observer2 = new IntersectionObserver((_0x1c54x12) => {
+            _0x1c54x12[_0x24e5[52]]((_0x1c54x13) => {
+                if (_0x1c54x13[_0x24e5[76]] > 0) {
+                    let _0x1c54xe = _0x1c54x13[_0x24e5[78]][_0x24e5[77]],
+                        _0x1c54xf = _0x1c54xe * 0.1;
+                    _0x1c54x13[_0x24e5[70]][_0x24e5[64]][_0x24e5[72]] = _0x24e5[86] + _0x1c54xf + _0x24e5[75]
+                }
+            })
+        });
+        document[_0x24e5[51]](_0x24e5[87])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer[_0x24e5[80]](_0x1c54x4)
+        });
+        document[_0x24e5[51]](_0x24e5[88])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer2[_0x24e5[80]](_0x1c54x4)
+        })
+    }
+
+    function _0x1c54x15() {
+        observer = new IntersectionObserver((_0x1c54x12) => {
+            _0x1c54x12[_0x24e5[52]]((_0x1c54x13) => {
+                if (_0x1c54x13[_0x24e5[76]] > 0) {
+                    setTimeout(() => {
+                        _0x1c54x13[_0x24e5[70]][_0x24e5[62]][_0x24e5[68]](_0x24e5[89])
+                    }, 200)
+                }
+            })
+        });
+        document[_0x24e5[51]](_0x24e5[54])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer[_0x24e5[80]](_0x1c54x4)
+        });
+        document[_0x24e5[51]](_0x24e5[90])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer[_0x24e5[80]](_0x1c54x4)
+        });
+        document[_0x24e5[51]](_0x24e5[91])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer[_0x24e5[80]](_0x1c54x4)
+        });
+        document[_0x24e5[51]](_0x24e5[92])[_0x24e5[52]]((_0x1c54x4) => {
+            return observer[_0x24e5[80]](_0x1c54x4)
+        })
+    }
+    _0x1c54x2();
+    _0x1c54x8();
+    _0x1c54x15();
+    window[_0x24e5[93]] = function () {
+        _0x1c54xd();
+        _0x1c54x11();
+        _0x1c54x14()
+    }
+}
